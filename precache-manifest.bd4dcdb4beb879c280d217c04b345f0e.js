04933ab7707a60bd45eb01ceb7afd0823367c041
Stephen Byrne <s.byrne6@gmail.com>
2020-06-17 11:52:34 +0100

    Updates

---
+++ precache-manifest.bd4dcdb4beb879c280d217c04b345f0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cbd9ad6045a96dece8bb1a4ba4e5988",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "4c9c697d1538cf4cafb2",
+    "url": "/tic-tac-toe/static/css/main.3854bfff.chunk.css"
+  },
+  {
+    "revision": "b29bc30fe9f6edee9dbb",
+    "url": "/tic-tac-toe/static/js/2.16bd6c81.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/tic-tac-toe/static/js/2.16bd6c81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c9c697d1538cf4cafb2",
+    "url": "/tic-tac-toe/static/js/main.d298d7ac.chunk.js"
+  },
+  {
+    "revision": "40929888a404a7396a7f",
+    "url": "/tic-tac-toe/static/js/runtime-main.061e70df.js"
+  }
+]);
